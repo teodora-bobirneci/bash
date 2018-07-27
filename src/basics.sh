facd2bc3959a44176157886c84a57e112773a5e5
teodora-bobirneci <teobobirneci@yahoo.com>
2018-07-27 17:37:57 +0300

    Added variables

--- src/basics.sh
+++ src/basics.sh
@@ -2,9 +2,12 @@
 
 echo "Hello World!"
 
+greeting="Welcome"
+user=$(whoami)
+day=$(date +%A)
 
-
-
+echo "$greeting back $user! Today is $day, which is the best day of the entire week."
+echo "Your Bash shell bersion is $BASH_VERSION. Enjoy!"
 
 
 
